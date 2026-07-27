0f7bc5dcd83db1cbd38d908e8aace2ce5aefc032
Aision Labs <aisionlabs.build@gmail.com>
2026-07-27 00:31:18 -0500

    Version VYBE favicon link to refresh browser caches

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -127,7 +127,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.ico?v=24.10.4", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "" },
       {
